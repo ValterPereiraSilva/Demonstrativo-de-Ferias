e3283f98b4f59643b60184236f0dff855a017e13
ValterSilva <valterpereira280667@gmail.com>
2019-09-14 18:44:43 -0300

    Atualizado 14/09/2019

--- Ferias/Program.cs
+++ Ferias/Program.cs
@@ -16,63 +16,71 @@ namespace Ferias
 
         static void Main(string[] args)
         {
-            DemonstrativoDeFerias calculo;
+            try
+            {
+                DemonstrativoDeFerias calculo;
 
-            Console.Write(" Digite o Nome do Funcionário: ");
-            string funcionario = Console.ReadLine();
-            Console.Write(" " + funcionario + " Quanto Você Ganha Por Hora: R$ ");
-            hora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write(" " + funcionario + " Você vai tirar as Férias Completas (s/n)? ");
-            char pergunta = char.Parse(Console.ReadLine());
-            if (pergunta == 's' || pergunta == 'S')
-            {
-                ferias = 220.00;
-                calculo = new DemonstrativoDeFerias(funcionario, ferias, horaExtra75, horaExtra100, adicional, hora, dependente);
-            }
-            else
-            {
-                ferias = 146.66666;
-                abonoPecuniario = 73.33333;
-                calculo = new DemonstrativoDeFerias(funcionario, ferias, horaExtra75, horaExtra100, adicional, hora, dependente, abonoPecuniario);
-            }
-            Console.Write(" " + funcionario + " Você Tem Reflexos sobre as Férias (s/n)?: ");
-            pergunta = char.Parse(Console.ReadLine());
-            if (pergunta == 's' || pergunta == 'S'){
-                Console.Write(" " + funcionario + " Você fez Hora Extra à 75% (s/n)?: ");
+                Console.Write(" Digite o Nome do Funcionário: ");
+                string funcionario = Console.ReadLine();
+                Console.Write(" " + funcionario + " Quanto Você Ganha Por Hora: R$ ");
+                hora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write(" " + funcionario + " Você vai tirar as Férias Completas (s/n)? ");
+                char pergunta = char.Parse(Console.ReadLine());
+                if (pergunta == 's' || pergunta == 'S')
+                {
+                    ferias = 220.00;
+                    calculo = new DemonstrativoDeFerias(funcionario, ferias, horaExtra75, horaExtra100, adicional, hora, dependente);
+                }
+                else
+                {
+                    ferias = 146.66666;
+                    abonoPecuniario = 73.33333;
+                    calculo = new DemonstrativoDeFerias(funcionario, ferias, horaExtra75, horaExtra100, adicional, hora, dependente, abonoPecuniario);
+                }
+                Console.Write(" " + funcionario + " Você Tem Reflexos sobre as Férias (s/n)?: ");
                 pergunta = char.Parse(Console.ReadLine());
                 if (pergunta == 's' || pergunta == 'S')
                 {
-                    Console.Write(" " + funcionario + " Informe quantas Horas Extra à 75% são: ");
-                    horaExtra75 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                    calculo = new DemonstrativoDeFerias(funcionario, ferias, abonoPecuniario, horaExtra75, horaExtra100, adicional, hora, dependente);
+                    Console.Write(" " + funcionario + " Você fez Hora Extra à 75% (s/n)?: ");
+                    pergunta = char.Parse(Console.ReadLine());
+                    if (pergunta == 's' || pergunta == 'S')
+                    {
+                        Console.Write(" " + funcionario + " Informe quantas Horas Extra à 75% são: ");
+                        horaExtra75 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        calculo = new DemonstrativoDeFerias(funcionario, ferias, abonoPecuniario, horaExtra75, horaExtra100, adicional, hora, dependente);
+                    }
+                    Console.Write(" " + funcionario + " Você fez Hora Extra à 100% (s/n)?: ");
+                    pergunta = char.Parse(Console.ReadLine());
+                    if (pergunta == 's' || pergunta == 'S')
+                    {
+                        Console.Write(" " + funcionario + " Informe quantas Horas Extra à 100% são: ");
+                        horaExtra100 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        calculo = new DemonstrativoDeFerias(funcionario, ferias, abonoPecuniario, horaExtra75, horaExtra100, adicional, hora, dependente);
+                    }
+                    Console.Write(" " + funcionario + " Você fez Hora de Adicional Noturno durante o Ano (s/n)?: ");
+                    pergunta = char.Parse(Console.ReadLine());
+                    if (pergunta == 's' || pergunta == 'S')
+                    {
+                        Console.Write(" " + funcionario + " Informe quantas Horas de Adicional Noturno são: ");
+                        adicional = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        calculo = new DemonstrativoDeFerias(funcionario, ferias, abonoPecuniario, horaExtra75, horaExtra100, adicional, hora, dependente);
+                    }
                 }
-                Console.Write(" " + funcionario + " Você fez Hora Extra à 100% (s/n)?: ");
+                Console.Write(" " + funcionario + " Você tem Dependentes para Dedução do IRRF (s/n)?: ");
                 pergunta = char.Parse(Console.ReadLine());
                 if (pergunta == 's' || pergunta == 'S')
                 {
-                    Console.Write(" " + funcionario + " Informe quantas Horas Extra à 100% são: ");
-                    horaExtra100 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write(" " + funcionario + " Informe quantos Dependentes são: ");
+                    dependente = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                     calculo = new DemonstrativoDeFerias(funcionario, ferias, abonoPecuniario, horaExtra75, horaExtra100, adicional, hora, dependente);
                 }
-                Console.Write(" " + funcionario + " Você fez Hora de Adicional Noturno durante o Ano (s/n)?: ");
-                pergunta = char.Parse(Console.ReadLine());
-                if (pergunta == 's' || pergunta == 'S')
-                {
-                    Console.Write(" " + funcionario + " Informe quantas Horas de Adicional Noturno são: ");
-                    adicional = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                    calculo = new DemonstrativoDeFerias(funcionario, ferias, abonoPecuniario, horaExtra75, horaExtra100, adicional, hora, dependente);
-                }
+                Console.WriteLine();
+                Console.WriteLine(calculo.ToString());
             }
-            Console.Write(" " + funcionario + " Você tem Dependentes para Dedução do IRRF (s/n)?: ");
-            pergunta = char.Parse(Console.ReadLine());
-            if (pergunta == 's' || pergunta == 'S')
+            catch (Exception e)
             {
-                Console.Write(" " + funcionario + " Informe quantos Dependentes são: ");
-                dependente = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                calculo = new DemonstrativoDeFerias(funcionario, ferias, abonoPecuniario, horaExtra75, horaExtra100, adicional, hora, dependente);
+                Console.WriteLine(e.Message);
             }
-            Console.WriteLine();
-            Console.WriteLine(calculo.ToString());
         }
     }
 }
